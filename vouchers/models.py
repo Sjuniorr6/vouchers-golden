bb173715a98535d8a092bc8eae6194122a7ffe90
Sjuniorr6 <sjuniorr6@gmail.com>
2025-04-21 16:46:42 -0300

    feito

--- vouchers/models.py
+++ vouchers/models.py
@@ -11,7 +11,6 @@ from datetime import timedelta
 from decimal import Decimal
 from django.utils import timezone
 
-
 class voucher(models.Model):
     motorista = models.CharField(max_length=50, null=True, blank=True)
     data = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -24,15 +23,20 @@ class voucher(models.Model):
     valor = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal('40.00'))
     qrcode_image = models.ImageField(upload_to='qrcodes', null=True, blank=True)
 
-    # Campos de auditoria
+    # auditoria
     updated_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
+        null=True, blank=True,
         on_delete=models.SET_NULL,
         related_name='vouchers_updated'
     )
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # guarda valores originais para evitar dupla subtração
+        self._initial_valor = self.valor
+        self._initial_gasto = self.gasto or Decimal('0')
 
     def __str__(self):
         return f"Voucher {self.pk}"
@@ -62,34 +66,27 @@ class voucher(models.Model):
             raise ValidationError("O valor não pode ficar negativo após subtrair o gasto.")
 
     def save(self, *args, user=None, **kwargs):
-        # Registra quem alterou valor ou gasto
+        # registra usuário em alteração de gasto/valor
         if self.pk and user is not None:
-            old = voucher.objects.get(pk=self.pk)
-            if (
-                (self.valor != old.valor) or
-                ((self.gasto or Decimal('0')) != (old.gasto or Decimal('0')))
-            ):
+            if (self.valor != self._initial_valor) or ((self.gasto or Decimal('0')) != self._initial_gasto):
                 self.updated_by = user
 
         creating = self.pk is None
         self.full_clean()
         super().save(*args, **kwargs)
 
-        # Gera QR code se for novo voucher
+        # gera QR code para novos
         if creating or not self.qrcode_image:
             domain = "http://www.gsvouchers.com.br"
-            edit_url = f"{domain}/vouchers/editar/{self.pk}/"
-
+            link = f"{domain}/vouchers/editar/{self.pk}/"
             qr = qrcode.QRCode(version=1, box_size=10, border=4)
-            qr.add_data(edit_url)
+            qr.add_data(link)
             qr.make(fit=True)
 
             img = qr.make_image(fill_color="black", back_color="white")
-            img_io = BytesIO()
-            img.save(img_io, 'PNG')
-            img_io.seek(0)
-
-            filename = f"qr_{uuid.uuid4().hex}.png"
-            self.qrcode_image.save(filename, File(img_io), save=False)
-
+            buf = BytesIO()
+            img.save(buf, 'PNG')
+            buf.seek(0)
+            name = f"qr_{uuid.uuid4().hex}.png"
+            self.qrcode_image.save(name, File(buf), save=False)
             super().save(update_fields=['qrcode_image'])
